a71a2a8e6d8a81015a129d302c065b48ad7720c0
pjohn1203 <33132277+pjohn1203@users.noreply.github.com>
2017-12-02 17:55:33 -0800

    Initial commit

---
+++ app/src/main/java/com/dealfaro/luca/serviceexample/MainActivity.java
@@ -0,0 +1,222 @@
+package com.dealfaro.luca.serviceexample;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+
+
+import com.dealfaro.luca.serviceexample.MyService.MyBinder;
+
+public class MainActivity extends ActionBarActivity
+    implements com.dealfaro.luca.serviceexample.MyServiceTask.ResultCallback {
+
+    public static final int DISPLAY_NUMBER = 10;
+    private Handler mUiHandler;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Rect mSurfaceSize;
+
+
+    private static final String LOG_TAG = "MainActivity";
+
+    // Service connection variables.
+    private boolean serviceBound;
+    private MyService myService;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mUiHandler = new Handler(getMainLooper(), new UiCallback());
+        serviceBound = false;
+        // Prevents the screen from dimming and going to sleep.
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Starts the service, so that the service will only stop when explicitly stopped.
+        Intent intent = new Intent(this, MyService.class);
+        startService(intent);
+        bindMyService();
+    }
+
+    private void bindMyService() {
+        // We are ready to show images, and we should start getting the bitmaps
+        // from the motion detection service.
+        // Binds to the service.
+        Log.i(LOG_TAG, "Starting the service");
+        Intent intent = new Intent(this, MyService.class);
+        Log.i("LOG_TAG", "Trying to bind");
+        bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+    }
+
+
+    // Service connection code.
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder serviceBinder) {
+            // We have bound to the camera service.
+            MyBinder binder = (MyBinder) serviceBinder;
+            myService = binder.getService();
+            serviceBound = true;
+            // Let's connect the callbacks.
+            Log.i("MyService", "Bound succeeded, adding the callback");
+            myService.addResultCallback(MainActivity.this);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            serviceBound = false;
+        }
+    };
+
+    @Override
+    protected void onPause() {
+        if (serviceBound) {
+            if (myService != null) {
+                myService.removeResultCallback(this);
+            }
+            Log.i("MyService", "Unbinding");
+            unbindService(serviceConnection);
+            serviceBound = false;
+            // If we like, stops the service.
+            if (true) {
+                Log.i(LOG_TAG, "Stopping.");
+                Intent intent = new Intent(this, MyService.class);
+                stopService(intent);
+                Log.i(LOG_TAG, "Stopped.");
+            }
+        }
+        super.onPause();
+    }
+
+    /**
+     * This function is called from the service thread.  To process this, we need
+     * to create a message for a handler in the UI thread.
+     */
+    @Override
+    public void onResultReady(ServiceResult result) {
+        if (result != null) {
+            Log.i(LOG_TAG, "Preparing a message for " + result.intValue);
+        } else {
+            Log.e(LOG_TAG, "Received an empty result!");
+        }
+        mUiHandler.obtainMessage(DISPLAY_NUMBER, result).sendToTarget();
+    }
+
+    /**
+     * This Handler callback gets the message generated above.
+     * It is used to display the integer on the screen.
+     */
+    private class UiCallback implements Handler.Callback {
+        @Override
+        public boolean handleMessage(Message message) {
+            if (message.what == DISPLAY_NUMBER) {
+                // Gets the result.
+                ServiceResult result = (ServiceResult) message.obj;
+                // Displays it.
+                if (result != null) {
+                    Log.i(LOG_TAG, "Displaying: " + result.intValue);
+                    if(result.intValue == 1) {
+                        TextView tv = (TextView) findViewById(R.id.number_view);
+                        tv.setText("The phone has moved");
+                    }
+
+                    TextView tv2 = (TextView)findViewById(R.id.textView);
+                    tv2.setText(Integer.toString(result.intValue));
+
+                    TextView tv3 = (TextView)findViewById(R.id.textView2);
+                    tv3.setText(Integer.toString(result.intValue));
+
+
+
+                    // Tell the worker that the bitmap is ready to be reused
+                    if (serviceBound && myService != null) {
+                        Log.i(LOG_TAG, "Releasing result holder for " + result.intValue);
+                        myService.releaseResult(result);
+                    }
+                } else {
+                    Log.e(LOG_TAG, "Error: received empty message!");
+                }
+            }
+            return true;
+        }
+    }
+
+
+    /*-------------------------------------------------------------------------
+    My Code added to Luca's Service
+
+    startService first unbinds the service then stops it, calls onResume to start it again
+
+    stopService unbinds and stops the service then closes the app
+    */
+
+    public void startService(View view){
+
+        unbindService(serviceConnection);
+        serviceBound = false;
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+        onResume();
+
+    }
+
+    public void stopService(View view){
+
+        unbindService(serviceConnection);
+        serviceBound = false;
+        Intent intent = new Intent(this, MyService.class);
+        stopService(intent);
+        //finish();
+        //System.exit(0);
+
+    }
+
+
+    /*--------------------------------------------------------------------------- */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/dealfaro/luca/serviceexample/MyServiceTask.java
@@ -0,0 +1,204 @@
+package com.dealfaro.luca.serviceexample;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.provider.Settings;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import static android.content.Context.SENSOR_SERVICE;
+
+
+/**
+ * Created by luca on 7/5/2015.
+ */
+
+
+public class MyServiceTask implements Runnable, SensorEventListener {
+
+    public static final String LOG_TAG = "MyService";
+    private boolean running;
+    private Context context;
+    //private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+    private int didItMove = 0;
+    Date initDate, currentDate;
+
+
+    private Set<ResultCallback> resultCallbacks = Collections.synchronizedSet(
+            new HashSet<ResultCallback>());
+    private ConcurrentLinkedQueue<ServiceResult> freeResults =
+            new ConcurrentLinkedQueue<ServiceResult>();
+
+    public MyServiceTask(Context _context) {
+        context = _context;
+        // Put here what to do at creation.
+    }
+
+
+    @Override
+    public void run() {
+
+
+        initDate = Calendar.getInstance().getTime();
+        Date tempDate = Calendar.getInstance().getTime();
+        long difftime = 0;
+        /*
+        while(difftime < 30){
+            tempDate = Calendar.getInstance().getTime();
+            difftime = (tempDate.getTime() - initDate.getTime())/1000;
+            System.out.println(difftime);
+        }*/
+
+        
+        running = true;
+        Random rand = new Random();
+        while (running) {
+            // Sleep a tiny bit.
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.getLocalizedMessage();
+            }
+
+
+            SM = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+
+            //Accelerometer Service
+
+            mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+            //Register Sensor Listener
+
+            SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+
+            // Generate a random number.
+            int r = rand.nextInt(100);
+            // Sends it to the UI thread in MainActivity (if MainActivity
+            // is running).
+            Log.i(LOG_TAG, "Sending random number: " + r);
+            //notifyResultCallback(didItMove);
+
+
+        }
+    }
+
+    public void addResultCallback(ResultCallback resultCallback) {
+        Log.i(LOG_TAG, "Adding result callback");
+        resultCallbacks.add(resultCallback);
+    }
+
+    public void removeResultCallback(ResultCallback resultCallback) {
+        Log.i(LOG_TAG, "Removing result callback");
+        // We remove the callback...
+        resultCallbacks.remove(resultCallback);
+        // ...and we clear the list of results.
+        // Note that this works because, even though mResultCallbacks is a synchronized set,
+        // its cardinality should always be 0 or 1 -- never more than that.
+        // We have one viewer only.
+        // We clear the buffer, because some result may never be returned to the
+        // free buffer, so using a new set upon reattachment is important to avoid
+        // leaks.
+        freeResults.clear();
+    }
+
+    // Creates result bitmaps if they are needed.
+    private void createResultsBuffer() {
+        // I create some results to talk to the callback, so we can reuse these instead of creating new ones.
+        // The list is synchronized, because integers are filled in the service thread,
+        // and returned to the free pool from the UI thread.
+        freeResults.clear();
+        for (int i = 0; i < 10; i++) {
+            freeResults.offer(new ServiceResult());
+        }
+    }
+
+    // This is called by the UI thread to return a result to the free pool.
+    public void releaseResult(ServiceResult r) {
+        Log.i(LOG_TAG, "Freeing result holder for " + r.intValue);
+        freeResults.offer(r);
+    }
+
+    public void stopProcessing() {
+        running = false;
+    }
+
+    public void setTaskState(boolean b) {
+        // Do something with b.
+    }
+
+    /**
+     * Call this function to return the integer i to the activity.
+     * @param i
+     */
+    private void notifyResultCallback(int i) {
+        if (!resultCallbacks.isEmpty()) {
+            // If we have no free result holders in the buffer, then we need to create them.
+            if (freeResults.isEmpty()) {
+                createResultsBuffer();
+            }
+            ServiceResult result = freeResults.poll();
+            // If we got a null result, we have no more space in the buffer,
+            // and we simply drop the integer, rather than sending it back.
+            if (result != null) {
+                result.intValue = i;
+                for (ResultCallback resultCallback : resultCallbacks) {
+                    Log.i(LOG_TAG, "calling resultCallback for " + result.intValue);
+                    resultCallback.onResultReady(result);
+                }
+            }
+        }
+    }
+
+
+    //OnSensorChanges checks if there is significant x or y acceleration.
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        double xVal = sensorEvent.values[0];
+        double yVal = sensorEvent.values[1];
+        currentDate = Calendar.getInstance().getTime();
+        long diffTime = (currentDate.getTime() - initDate.getTime())/1000;
+
+
+        if(xVal >= 2.5 || yVal >= 2.5){
+            didItMove = 1;
+            if(diffTime >= 30 && didItMove == 1) {
+                notifyResultCallback(didItMove);
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public interface ResultCallback {
+        void onResultReady(ServiceResult result);
+    }
+
+}
